9978621b45707bc60866789850a5b7949bc6251b
StanleyHung0908 <stanleyhung0908@gmail.com>
2018-01-03 02:34:22 -1000

    Add files via upload

---
+++ Games/Tank/Assets/SampleScenes/Scripts/NetworkLobbyHook.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Prototype.NetworkLobby;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+public class NetworkLobbyHook : LobbyHook 
+{
+    int ID = 1;
+    public static int totalPlayerCount = 0;
+    public static List<GameObject> clientDeathCount = new List<GameObject>();
+    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        LobbyPlayer lobby = lobbyPlayer.GetComponent<LobbyPlayer>();
+        TankColor tank = gamePlayer.GetComponent<TankColor>();
+        ClampName playerName = gamePlayer.GetComponentInChildren<ClampName>();
+        TankIdentity playerID = gamePlayer.GetComponent<TankIdentity>();
+        tank.color = lobby.playerColor;
+        playerName.nickName = lobby.playerName;
+        playerName.color = lobby.playerColor;
+        totalPlayerCount = ID;
+        playerID.setIdentity(ID++);
+    }
+}
